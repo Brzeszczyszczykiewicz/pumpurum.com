b4da48d46fa2dae63b989cb87b5b580439a5ee94
Brzeszczyszczykiewicz <azadorov1234@gmail.com>
2019-12-10 16:23:15 +0300

    correctly adding cart

--- cart/views.py
+++ cart/views.py
@@ -4,17 +4,19 @@ from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
 
+
 @require_POST
 def cart_add(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
-    form = CartAddProductForm(request.POST)
+    form = CartAddProductForm(request.POST, 'cart/form_add.html', {'form': form})
     if form.is_valid():
         cd = form.cleaned_data
         cart.add(product=product,
-                quantity=cd['quantity'],
-                update_quantity=cd['update'])
+                 quantity=cd['quantity'],
+                 update_quantity=cd['update'])
     return redirect('cart:cart_detail')
+
 
 def cart_remove(request, product_id):
     cart = Cart(request)
@@ -22,10 +24,11 @@ def cart_remove(request, product_id):
     cart.remove(product)
     return redirect('cart:cart_detail')
 
+
 def cart_detail(request):
     cart = Cart(request)
     for item in cart:
-        item['update_quantity_form'] = CartAddProductForm(
-            initial={'quantity': item['quantity'],
-                     'update': True})
+            item['update_quantity_form'] = CartAddProductForm(
+                              initial={'quantity': item['quantity'],
+                              'update': True})
     return render(request, 'cart/detail.html', {'cart': cart})
